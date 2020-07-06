784f57d29dba6e44a9d001ccc6c6eba1da2ec108
shajun <junco.sha@gmail.com>
2020-07-06 19:07:23 +0800

    Add Base Styles to Our App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React from "react";
-
-function App() {
-  return <div>app</div>;
+import React, { Component } from "react";
+import "./App.css";
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1 className="title">React Movie Search</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
