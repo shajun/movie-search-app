36fceba6b1de6d63c0d746849ff91abd48e7f285
shajun <junco.sha@gmail.com>
2020-07-07 18:37:09 +0800

    Create the Search Movies Function

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -1,8 +1,25 @@
 import React from "react";
 
 function SearchMovies() {
+  const searchMovies = async (e) => {
+    e.preventDefault();
+    console.log("submitting");
+
+    const query = "北京";
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=b1210020bfa1bcf17ffdc3343b69b3ee&language=zh-CN&query=${query}&page=1&include_adult=false`;
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={searchMovies}>
       <label className="label" htmlFor="query">
         Movie Name
       </label>
