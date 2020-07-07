2c47769219677b976978c55402c7aa1b25d4524d
shajun <junco.sha@gmail.com>
2020-07-07 10:57:46 +0800

    Create SearchMovies Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
+import SearchMovies from "./components/SearchMovies";
 class App extends Component {
   render() {
     return (
       <div className="container">
         <h1 className="title">React Movie Search</h1>
+        <SearchMovies />
       </div>
     );
   }
---
+++ src/test.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Test() {
+  return <h1>Hello World</h1>;
+}
+
+// to create the SearchMovie component
+// form with a class of form
+// label with htmlFor="query" and a class of Label
+// input to type text with a name of "query" and a placeholder
+// button class of button and a type of submit
