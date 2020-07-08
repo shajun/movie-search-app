6cb1d725c6bf7b0c32175d4553bbe2ef6edcfe0d
shajun <junco.sha@gmail.com>
2020-07-08 15:24:57 +0800

    Create the Movie Card Component (Challenge)

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import MovieCard from "./MovieCard";
 
 function SearchMovies() {
   // states input query, movies
@@ -45,23 +46,7 @@ function SearchMovies() {
         {movies
           .filter((movie) => movie.poster_path)
           .map((movie) => (
-            <div className="card" key={movie.id}>
-              <img
-                className="card--image"
-                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-                alt={movie.title + " poster"}
-              />
-              <div className="card--content">
-                <h3 className="card--title">{movie.title}</h3>
-                <p>
-                  <small>发布日期：{movie.release_date}</small>
-                </p>
-                <p>
-                  <small>评分：{movie.vote_average}</small>
-                </p>
-                <p className="card--desc">{movie.overview}</p>
-              </div>
-            </div>
+            <MovieCard movie={movie} key={movie.id} />
           ))}
       </div>
     </>
